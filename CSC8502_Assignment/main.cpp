3a38ce8419ac337bdbf212b8345f79e21447257d
CamusY <chienranyu@gmail.com>
2025-11-03 11:05:43 +0000

    completely rebuild the whole project

--- CSC8502_Assignment/main.cpp
+++ CSC8502_Assignment/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 #include <memory>
-#include "GLFWWindow.h"
-#include "IWindow.h"
-#include "IInputHandler.h"
+
 
 int main() {
-	std::unique_ptr<IWindow> window = std::make_unique<GLFWWindow>();
-	IInputHandler* inputHandler = dynamic_cast<IInputHandler*>(window.get());
-	if (!window->createWindow(800, 600, "CSC8502 Assignment")) {
-		std::cerr << "Failed to create window!" << std::endl;
-		return -1;
-	}
-	while (!window->shouldCloseWindow()) {
-		window->pollEvents();
-		window->swapBuffers();
-	}
+	std::cout << "Hello, World!" << std::endl;
 }
