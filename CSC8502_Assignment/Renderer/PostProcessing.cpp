8a4fa1820ebb3c214bdf0f0b46cd7c0785decf86
CamusY <chienranyu@gmail.com>
2025-11-10 16:40:08 +0000

    update: comment out debug output for post-processing framebuffer operations to reduce console noise

--- CSC8502_Assignment/Renderer/PostProcessing.cpp
+++ CSC8502_Assignment/Renderer/PostProcessing.cpp
@@ -34,7 +34,7 @@ void PostProcessing::BeginCapture() {
     if (!m_frameBuffer) {
         return;
     }
-    std::cerr << "[PostProcessing] Bind post-process FBO for scene capture" << std::endl;
+    //std::cerr << "[PostProcessing] Bind post-process FBO for scene capture" << std::endl;
     m_frameBuffer->Bind();
     glViewport(0, 0, m_width, m_height);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -44,7 +44,7 @@ void PostProcessing::EndCapture() {
     if (!m_frameBuffer) {
         return;
     }
-    std::cerr << "[PostProcessing] Unbind FBO after scene capture" << std::endl;
+    //std::cerr << "[PostProcessing] Unbind FBO after scene capture" << std::endl;
     m_frameBuffer->Unbind();
 }
 
@@ -52,7 +52,7 @@ void PostProcessing::Present() {
     if (!m_frameBuffer) {
         return;
     }
-    std::cerr << "[PostProcessing] Present captured texture to default framebuffer" << std::endl;
+    //std::cerr << "[PostProcessing] Present captured texture to default framebuffer" << std::endl;
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     glViewport(0, 0, m_width, m_height);
     GLboolean depthEnabled = GL_FALSE;
